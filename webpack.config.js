40ed16ad656ebcb08a5d3277bddfd8c6b99826d0
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-22 14:12:25 -0500

    created webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const Path = require('path')
+const HtmlWebpack = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: Path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.(css)$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.(png|jpg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+          {
+            loader: 'react-svg-loader',
+            options: {
+              jsx: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpack({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    proxy: {
+      '/xpose/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    },
+    historyApiFallback: true
+  }
+}
