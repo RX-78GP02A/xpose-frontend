148618b5a97a7ac846a037ca80858ecb044ff6b3
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-22 15:26:29 -0500

    created App.js

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import './assets/css/index.css';
+import Notifications from './components/Notifications';
+
+import Approute from './Routes/index';
+import Footer from './components/Footer'
+
+import { Provider as NotifcationProvider } from './context/Notification'
+import { Provider as AuthProvider } from './context/Auth'
+
+export default (props) => {
+  return (
+    <div>
+      <NotifcationProvider>
+        <Notifications />
+        <Router>
+          <AuthProvider>
+            <Approute />
+          </AuthProvider>
+        </Router>
+      </NotifcationProvider>
+      <Footer />
+    </div>
+  )
+}
