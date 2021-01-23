c4ce850f7e7792070603cac5e15d131f015a3f4b
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-22 22:42:34 -0500

    updated actionTypes.js

--- src/context/actionTypes.js
+++ src/context/actionTypes.js
@@ -3,11 +3,11 @@ export const ADD_NOTI = 'ADD_NOTI';
 export const REMOVE_NOTI = 'REMOVE_NOTI';
 
 // Post action types
-export const SET_POSTS = 'SET_POSTS'
-export const ADD_POST = 'ADD_POST'
-export const LIKE_POST = 'LIKE_POST'
-export const UNLIKE_POST = 'UNLIKE_POST'
-export const ADD_COMMENT = 'ADD_COMMENT'
+export const SET_POSTS = 'SET_POSTS';
+export const ADD_POST = 'ADD_POST';
+export const LIKE_POST = 'LIKE_POST';
+export const UNLIKE_POST = 'UNLIKE_POST';
+export const ADD_COMMENT = 'ADD_COMMENT';
 export const SET_COMMENTS = 'SET_COMMENTS'
 export const DELETE_COMMENT = 'DELETE_COMMENT'
 export const DELETE_POST = 'DELETE_POST'
