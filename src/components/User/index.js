b1bc547c583cbe3e01a7da2df184e8904935bd90
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-22 23:59:12 -0500

    created index.js

---
+++ src/components/User/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Avatar from '../../assets/images/avatar.jpg';
+import './style.css';
+import { Link } from 'react-router-dom';
+
+export default ({ user }) => {
+  return (
+    <div className='user-row'>
+      <div className='user-img'>
+        <img src={user.avatar || Avatar} alt={user.username} />
+      </div>
+      <Link to={`/${user.username}`}>
+        {user.firstname} {user.lastname}
+      </Link>
+    </div>
+  )
+};
