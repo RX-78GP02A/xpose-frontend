441943ee5c237d12d648ac46090200fb9e607538
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-23 00:59:24 -0500

    updated index.js

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './style.css'
+
+export default () => {
+  return (
+    <div>
+      <p className='footer'>
+        Developed by <a href='https://github.com/RX-78GP02A'>Marcus Ridley</a>
+      </p>
+    </div>
+  )
+}
