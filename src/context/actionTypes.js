e9b318eca091b8b49c879872dd359402eecb4b4f
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-22 22:47:49 -0500

    updated avatar.jpg

--- src/context/actionTypes.js
+++ src/context/actionTypes.js
@@ -8,20 +8,20 @@ export const ADD_POST = 'ADD_POST';
 export const LIKE_POST = 'LIKE_POST';
 export const UNLIKE_POST = 'UNLIKE_POST';
 export const ADD_COMMENT = 'ADD_COMMENT';
-export const SET_COMMENTS = 'SET_COMMENTS'
-export const DELETE_COMMENT = 'DELETE_COMMENT'
-export const DELETE_POST = 'DELETE_POST'
-export const RESET = 'RESET'
+export const SET_COMMENTS = 'SET_COMMENTS';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+export const DELETE_POST = 'DELETE_POST';
+export const RESET = 'RESET';
 
 // Profile action types
-export const SET_PROFILE = 'SET_PROFILE'
-export const ADD_FOLLOWER = 'ADD_FOLLOWER'
-export const REMOVE_FOLLOWER = 'REMOVE_FOLLOWER'
-export const SET_FOLLOWERS = 'SET_FOLLOWERS'
-export const SET_FOLLOWING = 'SET_FOLLOWING'
-export const UPDATE_PROFILE = 'UPDATE_PROFILE'
+export const SET_PROFILE = 'SET_PROFILE';
+export const ADD_FOLLOWER = 'ADD_FOLLOWER';
+export const REMOVE_FOLLOWER = 'REMOVE_FOLLOWER';
+export const SET_FOLLOWERS = 'SET_FOLLOWERS';
+export const SET_FOLLOWING = 'SET_FOLLOWING';
+export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 
 // Authentication
-export const LOGIN_USER = 'LOGIN_USER'
-export const LOGOUT_USER = 'LOGOUT_USER'
-export const UPDATE_AUTH_USER = 'UPDATE_AUTH_USER'
+export const LOGIN_USER = 'LOGIN_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
+export const UPDATE_AUTH_USER = 'UPDATE_AUTH_USER';
