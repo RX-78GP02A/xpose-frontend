fef1cfb4e489e7e9f9f86c8fab05603b8f4c2f4f
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-22 21:12:07 -0500

    Profile.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './assets/css/index.css';
 import Notifications from './components/Notifications';
 
 import Approute from './Routes/index';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 
 import { Provider as NotifcationProvider } from './context/Notification'
-import { Provider as AuthProvider } from './context/Auth'
+import { Provider as AuthProvider } from './context/Auth';
 
 export default (props) => {
   return (
@@ -24,4 +24,4 @@ export default (props) => {
       <Footer />
     </div>
   )
-}
+};
