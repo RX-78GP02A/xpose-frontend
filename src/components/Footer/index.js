be555cb9ce107f693745f42d00b6a48f3044d043
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-23 18:57:21 -0500

    updated image containers

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from 'react'
+import GitHub from '../Images/GitHub.png';
+import Envelope from '../Images/Envelope.png';
+import LinkedIn from '../Images/LinkedIn.png';
 
-import './style.css';
+import './style.css'
 
 export default () => {
   return (
     <div>
       <p className='footer'>
-        Developed by <a href='https://github.com/RX-78GP02A'>Marcus Ridley</a>
+    
+      <a href="https://https://www.linkedin.com/in/marcus-ridley/"><img className="linkedIn" src={LinkedIn} alt="LinkedIn"/></a>
+      <a href="https://github.com/RX-78GP02A"><img className="github" src={GitHub} alt="GitHub"/></a>
+      <a href="mailto:mridley@knights.ucf.edu"><img className="envelope" src={Envelope} alt="Envelope"/></a>
       </p>
     </div>
   )
-};
+}
