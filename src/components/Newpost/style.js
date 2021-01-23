e3b53b6633e95f0c6cb3691f496ef48eecd8e8c0
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-23 00:42:31 -0500

    created preview-image.js

---
+++ src/components/Newpost/style.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const Caption = styled.textarea`
+  outline: none;
+  border: none;
+  resize: none;
+  font-size: 0.9em;
+  letter-spacing: 0.02em;
+  font-family: 'Roboto', sans-serif;
+`
+
+export const PreviewCont = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  @media (max-width: 450px) {
+    margin-bottom: 10px;
+  }
+`
+
+export const ImgContainer = styled.div`
+  position: relative;
+  width: 120px;
+  height: 120px;
+  margin: 0 10px 10px 0;
+  overflow: hidden;
+  @media (max-width: 450px) {
+    width: 46%;
+    margin: 5px;
+  }
+`
+
+export const Img = styled.img`
+  width: 100%;
+  object-fit: cover;
+`
+
+export const Icon = styled(FontAwesomeIcon)`
+  font-size: ${(props) => props.fontSize};
+  color: #fff;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  margin: 0;
+  cursor: pointer;
+`
+export const P = styled.p`
+  font-size: 0.8rem;
+  letter-spacing: 0.01em;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 1em;
+  margin-left: 0.2em;
+`
