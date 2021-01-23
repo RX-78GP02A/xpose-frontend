cee0dd617f1852d5f748a766358826d1fa242836
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-22 21:18:54 -0500

    created actionTypes.js

---
+++ src/context/actionTypes.js
@@ -0,0 +1,27 @@
+// Notification action types
+export const ADD_NOTI = 'ADD_NOTI';
+export const REMOVE_NOTI = 'REMOVE_NOTI';
+
+// Post action types
+export const SET_POSTS = 'SET_POSTS'
+export const ADD_POST = 'ADD_POST'
+export const LIKE_POST = 'LIKE_POST'
+export const UNLIKE_POST = 'UNLIKE_POST'
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const SET_COMMENTS = 'SET_COMMENTS'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const DELETE_POST = 'DELETE_POST'
+export const RESET = 'RESET'
+
+// Profile action types
+export const SET_PROFILE = 'SET_PROFILE'
+export const ADD_FOLLOWER = 'ADD_FOLLOWER'
+export const REMOVE_FOLLOWER = 'REMOVE_FOLLOWER'
+export const SET_FOLLOWERS = 'SET_FOLLOWERS'
+export const SET_FOLLOWING = 'SET_FOLLOWING'
+export const UPDATE_PROFILE = 'UPDATE_PROFILE'
+
+// Authentication
+export const LOGIN_USER = 'LOGIN_USER'
+export const LOGOUT_USER = 'LOGOUT_USER'
+export const UPDATE_AUTH_USER = 'UPDATE_AUTH_USER'
